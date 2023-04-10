9303a5316405d4a791c22e8e5ed50e949fe2c061
Darkrany <101005069+Darkrany@users.noreply.github.com>
2023-04-10 15:29:51 +0200

    paginacion

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
+const pokedex = document.getElementById('pokedex'); /* recogemos los datos del html */
+const cantidadElementos = document.getElementById("cantidad");
+const apiURL = `https://pokeapi.co/api/v2/pokemon/`; 
 
+const fetchPokemon = (limit, offset) => {
+                    fetch(apiURL + '?limit=' + limit + '&offset=' + offset)
+                    .then((res) => res.json())
+                    .then((data) => { 
+                        const pkm = data.results.map((pokemons) => ({
+                              name: pokemons.name,
+                              url: pokemons.url
+                          }
+                          
+                        )
+                      )
+                     pokemonListDisplay(pkm);
+                    }
+                  )
 
-const fetchPokemon = () => {
-    
-    const api = `https://pokeapi.co/api/v2/pokemon/`; 
- 
-                    fetch(api).then((res) => res.json())
-                    .then((data) => {
-                        const pkm = data.results.map((pokemon) => ({
-                          id: pokemon.id,
-                          name: pokemon.name,
-                        }));
-                        console.log(pkm);
-                      })                 
-
- console.log(api)
-         
-    
-
-     
 };
 
+const pokemonListDisplay = (pkm) => {
+        const printPokelist = pkm.map(
+          pokelist => 
+            `
+              <ul> 
+                <li>${pokelist.name}</li>
+              </ul>
+            `
+         ).join('');// Unir los elementos de la lista en una sola cadena de HTML
+    pokedex.innerHTML = printPokelist;
+ }
+ 
+
+cantidadElementos.addEventListener('change', () => {
+  const cantidad = cantidadElementos.value;
+  const offset = 0; 
+  fetchPokemon(cantidad, offset);
+});
 
 
 
-fetchPokemon()
+fetchPokemon(cantidadElementos.value, 0)
