e957a2e397b783c3aea605a2777801228cd794b5
Darkrany <101005069+Darkrany@users.noreply.github.com>
2023-04-10 18:06:06 +0200

    imgs

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 
+<<<<<<< Updated upstream
+=======
+const fetchPokemon = (limit, offset) => {
+                    fetch(apiURL + '?limit=' + limit + '&offset=' + offset)
+                    .then((res) => res.json())
+                    .then((data) => { 
+                        const pkm = data.results.map((pokemons, index) => ({
+                              id: index +1,
+                              name: pokemons.name,
+                              url: pokemons.url,
+                              img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+ parseInt(index+1) +".png"
+                          }
+                         
+                        )
+                      )
+                     pokemonListDisplay(pkm);
+                    }
+                  )
+>>>>>>> Stashed changes
 
 const fetchPokemon = () => {
     
@@ -20,6 +39,30 @@ const fetchPokemon = () => {
      
 };
 
+<<<<<<< Updated upstream
+=======
+const pokemonListDisplay = (pkm) => {
+        const printPokelist = pkm.map(
+          pokelist => 
+            `
+            <a class="card-container" href="">
+            <span class="poke-number">#${pokelist.id}</span>
+            <img src="${pokelist.img}" alt="${pokelist.name}">
+        
+            <h2 class="poke-name">${pokelist.name}</h2>
+            
+            `
+         ).join('');// Unir los elementos de la lista en una sola cadena de HTML
+    pokedex.innerHTML = printPokelist;
+ }
+ 
+
+cantidadElementos.addEventListener('change', () => {
+  const cantidad = cantidadElementos.value;
+  const offset = 0; 
+  fetchPokemon(cantidad, offset);
+});
+>>>>>>> Stashed changes
 
 
 
